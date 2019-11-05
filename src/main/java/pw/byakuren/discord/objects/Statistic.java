495b349e4783482ba51661608d3ee714b2da1ba5
Brod8362 <brodriguez410@gmail.com>
2019-11-05 16:35:35 -0600

    improve statistic enum

--- src/main/java/pw/byakuren/discord/objects/Statistic.java
+++ src/main/java/pw/byakuren/discord/objects/Statistic.java
@@ -1,5 +1,22 @@
 package pw.byakuren.discord.objects;
 
 public enum Statistic {
-    REACTIONS_SENT, REACTIONS_RECEIVED, MESSAGES_SENT, MESSAGES_DELETED, ATTACHMENTS_SENT;
+
+    REACTIONS_SENT("reactions_tx"), REACTIONS_RECEIVED("reactions_rx"),
+    MESSAGES_SENT("messages_sent"), MESSAGES_DELETED("messages_deleted"),
+    ATTACHMENTS_SENT("attachments_sent");
+
+    public final String datapoint_name;
+
+    Statistic(String s) {
+        datapoint_name = s;
+    }
+
+    public static Statistic datapointToStatistic(String str) {
+        for (Statistic s: Statistic.values()) {
+            if (s.datapoint_name.equals(str)) return s;
+        }
+        return null;
+    }
+
 }
