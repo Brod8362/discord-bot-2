a786600f8d6ea0ecdd949c3eaca76a9d61edfa01
Brod8362 <brodriguez410@gmail.com>
2019-11-05 10:36:59 -0600

    placeholder settings factory

---
+++ src/main/java/pw/byakuren/discord/objects/cache/factories/ServerSettingsFactory.java
@@ -0,0 +1,23 @@
+package pw.byakuren.discord.objects.cache.factories;
+
+import pw.byakuren.discord.DatabaseManager;
+import pw.byakuren.discord.objects.cache.datatypes.ServerSettings;
+
+import java.util.List;
+
+public class ServerSettingsFactory extends DatatypeFactory<ServerSettings> {
+
+    public ServerSettingsFactory(long serverid, DatabaseManager dbmg) {
+        super(serverid, dbmg);
+    }
+
+    @Override
+    public List<ServerSettings> getAll() {
+        throw new UnsupportedOperationException("unimplemented");
+    }
+
+    @Override
+    public ServerSettings get(Object... qualifiers) {
+        throw new UnsupportedOperationException("unimplemented");
+    }
+}
