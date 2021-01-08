6c1952198a89cbbd927cd635cfd6357011fda771
Brod8362 <brod8362@gmail.com>
2021-01-08 15:34:03 -0600

    Make MessageAction extend CacheEntry, add minor documentation

--- src/main/java/pw/byakuren/discord/filteraction/MessageAction.java
+++ src/main/java/pw/byakuren/discord/filteraction/MessageAction.java
@@ -1,15 +1,22 @@
 package pw.byakuren.discord.filteraction;
 
 import net.dv8tion.jda.api.entities.Message;
+import pw.byakuren.discord.DatabaseManager;
 import pw.byakuren.discord.filteraction.arguments.Argument;
+import pw.byakuren.discord.objects.cache.datatypes.CacheEntry;
 import pw.byakuren.discord.util.ScalaReplacements;
 
 import java.util.Arrays;
 
-public abstract class MessageAction implements Action<Message> {
+public abstract class MessageAction extends CacheEntry implements Action<Message> {
 
     public abstract String getName();
 
+    /**
+     * This method should return a string that can be parsed back into another Action with identical behavior,
+     * e.g. passing the output of getDisplay() into parseFromString() should work and behave the same.
+     * @return string representation of this action
+     */
     public String getDisplay() {
         return String.format("%s<%s>", getName(), getArgumentsDisplay());
     }
@@ -26,6 +33,16 @@ public abstract class MessageAction implements Action<Message> {
 
     public final MessageAction fromString(String s) {
         return parseFromString(s.substring(getName().length() + 1, s.lastIndexOf(">")));
+    }
+
+    @Override
+    protected void write(DatabaseManager dbmg) {
+        //TODO implement
+    }
+
+    @Override
+    protected void delete(DatabaseManager dbmg) {
+        //TODO implement
     }
 
     @Override
